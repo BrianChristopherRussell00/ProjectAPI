7c283f8d67174c766a0daaaa93931b90817dec15
Brian Russell <Brianrussell2017@yahoo.com>
2025-01-09 12:04:34 -0500

    Update 1/9/25 DTOs

---
+++ Controllers/RegionsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectAPI.Data;
+using ProjectAPI.Models.DTOs;
+
+namespace ProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly BrianRussellDbContext dbContext;
+
+        public RegionsController(BrianRussellDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regionsDomain = dbContext.Regions.ToList();
+
+            var regionsDto = new List<RegionDto>();
+            foreach (var regionDomain in regionsDomain)
+            {
+                regionsDto.Add(new RegionDto()
+                {
+                    Id = regionDomain.Id,
+                    Code = regionDomain.Code,
+                    Name = regionDomain.Name,
+                    RegionImageUrl = regionDomain.RegionImageUrl
+
+                });
+            }//Return DTOs
+                return Ok(regionsDto);
+            }
+  
+
+            [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute]Guid id)
+        {// var region = dbContext.Regions.FirstOrDefault(x=> x.Id == id);
+            // Get region domain model from database
+            var regionDomain = dbContext.Regions.Find(id);
+            if (regionDomain == null)
+            {
+                return NotFound();
+            }//Map/Convert Region Domain Model to Region DTO
+            var regionDto = new RegionDto()
+            {
+
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageUrl = regionDomain.RegionImageUrl
+            };
+            //Return DTO back to client 
+            return Ok(regionDto);
+        }
+    }
+}
